113ba74b4203d221a23d62b1d9802baed16733b3
radj307 <radj307@gmail.com>
2022-01-04 03:16:46 -0500

    update

--- AutoVersion.cmake
+++ AutoVersion.cmake
@@ -1,16 +1,31 @@
 # 307lib/cmake/modules/AutoVersion.cmake
 # Contains functions for automatically setting package versions from git tags
+#
+## USAGE
+# 1.	Add this directory to the CMAKE_MODULE_PATH list by replacing <PATH> in this line:
+#			list(APPEND CMAKE_MODULE_PATH "<PATH>")
+#
+# 2.	Retrieve the current git tag & use ${AUTOVERSION_PARSE_REGEX} to parse the version number from it
+#			GET_VERSION("${CMAKE_SOURCE_DIR}" MAJOR_VERSION MINOR_VERSION PATCH_VERSION)
+#
+# (optional):
+#
+# 3.	Set the version of your project in CMake to support CMake's packaging functionality
+#			MAKE_VERSION(FULL_VERSION ${VERSION_MAJOR} ${VERSION_MINOR} ${VERSION_PATCH})
+#
+# 4.	Create a version header containing C++ preprocessor definitions to support project-wide versioning from a single source.
+#			CREATE_VERSION_HEADER(<TARGET>, ${VERSION_MAJOR} ${VERSION_MINOR} ${VERSION_PATCH})
+#
 cmake_minimum_required(VERSION 3.19)
 
-set(AUTOVERSION_FIND_REGEX "^[vV]*([0-9]+)\.([0-9]+)\.([0-9]+)(.+)" CACHE STRING "Used by the AutoVersion PARSE_VERSION_STRING function to parse a git tag into a library version, by using capture groups. (1: Major Version ; 2: Minor Version ; 3: Patch Version ; 4: SHA1). All groups except for group 1 should be optional." FORCE)
+set(AUTOVERSION_PARSE_REGEX "^[vV]*([0-9]+)\.([0-9]+)\.([0-9]+).+" CACHE STRING "Used by the AutoVersion PARSE_VERSION_STRING function to parse a git tag into a library version, by using capture groups. (1: Major Version ; 2: Minor Version ; 3: Patch Version ; 4: SHA1). All groups except for group 1 should be optional." FORCE)
 
-#### PARSE_VERSION_STRING ####
+#### GET_VERSION ####
 # @brief				Retrieve and parse the result of calling `git describe --tags --dirty=-d`
 # @param _out_major		Variable name to store the major version number
 # @param _out_minor		Variable name to store the minor version number
 # @param _out_patch		Variable name to store the patch version number
-# @param _out_extra		Variable name to store extra characters in the version number
-function(parse_version_string _working_dir _out_major _out_minor _out_patch _out_extra)
+function(GET_VERSION _working_dir _out_major _out_minor _out_patch)
 	# set the "VERSION_TAG" cache variable to the result of `git describe ${AUTOVERSION_GIT_DESCRIBE_ARGS}` in CMAKE_SOURCE_DIR
 	execute_process(
 		COMMAND 
@@ -27,39 +42,34 @@ function(parse_version_string _working_dir _out_major _out_minor _out_patch _out
 	endif()
 
 	# Parse the version string using the provided regular expression
-	string(REGEX REPLACE ${AUTOVERSION_FIND_REGEX} "\\1" _MAJOR ${VERSION_TAG}) # get Major
-	string(REGEX REPLACE ${AUTOVERSION_FIND_REGEX} "\\2" _MINOR ${VERSION_TAG}) # get Minor
-	string(REGEX REPLACE ${AUTOVERSION_FIND_REGEX} "\\3" _PATCH ${VERSION_TAG}) # get Patch
-	string(REGEX REPLACE ${AUTOVERSION_FIND_REGEX} "\\4" _EXTRA ${VERSION_TAG}) # get Extra
+	string(REGEX REPLACE ${AUTOVERSION_PARSE_REGEX} "\\1" _MAJOR ${VERSION_TAG}) # get Major
+	string(REGEX REPLACE ${AUTOVERSION_PARSE_REGEX} "\\2" _MINOR ${VERSION_TAG}) # get Minor
+	string(REGEX REPLACE ${AUTOVERSION_PARSE_REGEX} "\\3" _PATCH ${VERSION_TAG}) # get Patch
 
 	# Print a status message showing the parsed values
 	message(STATUS "Successfully parsed version number from git tags.")
 	message(STATUS "  MAJOR: ${_MAJOR}")
 	message(STATUS "  MAJOR: ${_MINOR}")
 	message(STATUS "  MAJOR: ${_PATCH}")
-	message(STATUS "  EXTRA: ${_EXTRA}")
 
 	# Set the provided output variable names to the parsed version numbers
-	set(${_out_major} ${_MAJOR} CACHE STRING "(SEMVER) Major Version portion of the current git tag" FORCE)
-	set(${_out_minor} ${_MINOR} CACHE STRING "(SEMVER) Minor Version portion of the current git tag" FORCE)
-	set(${_out_patch} ${_PATCH} CACHE STRING "(SEMVER) Patch Version portion of the current git tag" FORCE)
-	set(${_out_extra} ${_EXTRA} CACHE STRING "(SEMVER) Everything that appears after the first 3 (period-separated) digits in the current git tag." FORCE)
-
+	set(${_out_major} "${_MAJOR}" CACHE STRING "(SEMVER) Major Version portion of the current git tag" FORCE)
+	set(${_out_minor} "${_MINOR}" CACHE STRING "(SEMVER) Minor Version portion of the current git tag" FORCE)
+	set(${_out_patch} "${_PATCH}" CACHE STRING "(SEMVER) Patch Version portion of the current git tag" FORCE)
+	
 	# Cleanup
 	unset(_MAJOR CACHE)
 	unset(_MINOR CACHE)
 	unset(_PATCH CACHE)
-	unset(_EXTRA CACHE)
 endfunction()
 
-#### CONCAT_VERSION_STRING ####
+#### MAKE_VERSION ####
 # @brief			Concatenate version strings into a single version string in the format "${MAJOR}.${MINOR}.${PATCH}${EXTRA}"
-# @param _out_ver	Name of the variable to use for output.
+# @param _out_var	Name of the variable to use for output.
 # @param _major		Major version number
 # @param _minor		Minor version number
 # @param _patch		Patch version number
-# @param _extra		Extra portions of the version string.
-function(concat_version_string _out_ver _major _minor _patch)
+function(MAKE_VERSION _out_var _major _minor _patch)
 	set(${_out_ver} "${_major}.${_minor}.${_patch}" CACHE STRING "Full version string." FORCE)
 endfunction()
 
@@ -70,7 +80,7 @@ endfunction()
 # @param _major			Major version number
 # @param _minor			Minor version number
 # @param _patch			Patch version number
-function(create_version_header _name _major _minor _patch)
+function(CREATE_VERSION_HEADER _name _major _minor _patch)
 	set(IN_NAME ${_name} CACHE STRING "" FORCE)
 	set(IN_MAJOR ${_major} CACHE STRING "" FORCE)
 	set(IN_MINOR ${_minor} CACHE STRING "" FORCE)
@@ -84,11 +94,13 @@ function(create_version_header _name _major _minor _patch)
 			endif()
 		endforeach()
 	elseif(EXISTS "${CMAKE_SOURCE_DIR}/cmake/307modules/version.h.in")
-		set(VERSION_IN_PATH "${CMAKE_SOURCE_DIR}/cmake/307modules/version.h.in")
-	elseif(EXISTS "${CMAKE_SOURCE_DIR}/307modules/version.h.in")
-		set(VERSION_IN_PATH "${CMAKE_SOURCE_DIR}/307modules/version.h.in")
+		set(VERSION_IN_PATH "${CMAKE_SOURCE_DIR}/cmake/307modules/input/version.h.in")
+	elseif(EXISTS "${CMAKE_SOURCE_DIR}/307modules/input/version.h.in")
+		set(VERSION_IN_PATH "${CMAKE_SOURCE_DIR}/307modules/input/version.h.in")
 	elseif(EXISTS "${CMAKE_SOURCE_DIR}/cmake/modules/version.h.in")
 		set(VERSION_IN_PATH "${CMAKE_SOURCE_DIR}/cmake/modules/version.h.in")
+	elseif(EXISTS "${CMAKE_SOURCE_DIR}/cmake/input/version.h.in")
+		set(VERSION_IN_PATH "${CMAKE_SOURCE_DIR}/cmake/input/version.h.in")
 	else()
 		message(FATAL_ERROR "AutoVersion.cmake cannot locate a valid version.h.in template file! You can specify the path to the target file as an additional parameter to CREATE_VERSION_HEADER if it isn't located at the default path.")
 	endif()
