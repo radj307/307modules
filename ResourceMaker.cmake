e00908672d91bfa70bab7fe65bbdb80b6131b441
radj307 <radj307@gmail.com>
2022-01-20 01:36:15 -0500

    add some ResourceMaker functions

--- ResourceMaker.cmake
+++ ResourceMaker.cmake
@@ -1,7 +1,28 @@
 # 307modules/ResourceMaker.cmake
 cmake_minimum_required(VERSION 3.20)
 
-set(RCM_ICON_RESOURCE "IDI_ICON1               ICON                    \"@_icon_filepath@\"\n" CACHE STRING "The content of an icon resource file, before using string(CONFIGURE) on it.")
+set(RCM_ICON_RESOURCE "IDI_ICON1               ICON                    \"@_icon_filepath@\"" CACHE STRING "The content of an icon resource file, before using string(CONFIGURE) on it.")
+
+macro(SETCACHE _name _value)
+	set("${_name}" "${_value}" CACHE STRING "" FORCE)
+endmacro()
+
+
+function(CREATE_FILE _out_file)
+	file(WRITE "${_out_file}" ${ARGN})
+endfunction()
+
+function(APPEND_FILE _out_file)
+	file(APPEND "${_out_file}" ${ARGN})
+endfunction()
+
+function(CREATE_FILE_VERSION_HEADER _out_file _name _version)
+	include(AutoVersion)
+	CREATE_FILE("${_out_file}" 
+		"#pragma once\\n"
+		"#define ${_name} \"${_version}\"\\n"
+	)
+endfunction()
 
 #### CREATE_RESOURCE(<OUT_FILEPATH> [CONTENT...]) ####
 function(CREATE_RESOURCE _out_rc_file)
@@ -38,17 +59,21 @@ function(CREATE_VERSION_RESOURCE
 )
 	# Set input variables
 	get_filename_component(IN_MY_FILENAME "${_out_rc_file}" NAME CACHE)
-	set(IN_VERSION_MAJOR "${_version_major}" CACHE STRING "" FORCE)
-	set(IN_VERSION_MINOR "${_version_minor}" CACHE STRING "" FORCE)
-	set(IN_VERSION_PATCH "${_version_patch}" CACHE STRING "" FORCE)
-	set(IN_COMPANYNAME "${_company_name}" CACHE STRING "" FORCE)
-	set(IN_FILEDESCRIPTION "${_file_description}" CACHE STRING "" FORCE)
-	set(IN_INTERNALNAME "${_internal_name}" CACHE STRING "" FORCE)
-	set(IN_LEGALCOPYRIGHT "${_legal_copyright}" CACHE STRING "" FORCE)
-	set(IN_ORIGINALFILENAME "${_original_filename}" CACHE STRING "" FORCE)
-	set(IN_PRODUCTNAME "${_product_name}" CACHE STRING "" FORCE)
+	SETCACHE(IN_VERSION_MAJOR "${_version_major}")
+	SETCACHE(IN_VERSION_MINOR "${_version_minor}")
+	SETCACHE(IN_VERSION_PATCH "${_version_patch}")
+	SETCACHE(IN_COMPANYNAME "${_company_name}")
+	SETCACHE(IN_FILEDESCRIPTION "${_file_description}")
+	SETCACHE(IN_INTERNALNAME "${_internal_name}")
+	SETCACHE(IN_LEGALCOPYRIGHT "${_legal_copyright}")
+	SETCACHE(IN_ORIGINALFILENAME "${_original_filename}")
+	SETCACHE(IN_PRODUCTNAME "${_product_name}")
 	file(REMOVE "${_out_rc_file}")
-	configure_file("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/input/target.rc.in" "${CMAKE_CURRENT_BINARY_DIR}/temp.rc" @ONLY) # Configure file
+
+	set(RESOURCE_INPUT_FILE "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/input/target.rc.in")
+
+	configure_file("${RESOURCE_INPUT_FILE}" "${CMAKE_CURRENT_BINARY_DIR}/temp.rc" @ONLY) # Configure file
+
 	include(CatFile)
 	CAT_FILE("${_out_rc_file}" "${CMAKE_CURRENT_BINARY_DIR}/temp.rc") # Concatenate the contents of temp.rc to the target file
 	file(REMOVE "${CMAKE_CURRENT_BINARY_DIR}/temp.rc") # Clean up temp.rc
