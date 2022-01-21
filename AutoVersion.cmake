3306e6be0847de5ed9b2c2d30644f80dfc7ac45a
radj307 <radj307@gmail.com>
2022-01-21 13:48:06 -0500

    Update autoversion & prependeach with PARENT_SCOPE

--- AutoVersion.cmake
+++ AutoVersion.cmake
@@ -32,6 +32,8 @@ set(AUTOVERSION_PARSE_REGEX
 	"Used by AutoVersion.cmake functions to parse the most recent git tag for a version number."
 )
 
+set(AUTOVERSION_2_REGEX_VALIDTAGCHARACTERS "[0-9A-Za-z]+" CACHE STRING "Regular expression used by the AV2 functions to retrieve all contiguous words/numbers from the current git tag. This should include letters.")
+
 #### GET_GIT_TAG(<REPO_DIR> <OUT_VAR>) ####
 # @brief				Call "git describe --tags" from the given directory, and set ${_out_var} to the result.
 # @param _working_dir	The root repository directory to call the git describe command in.
@@ -87,33 +89,6 @@ function(AV_GET_VERSION _working_dir _out_tag)
 	AV_PARSE_TAG("${${_out_tag}}" ${ARGN})
 endfunction()
 
-#### CREATE_VERSION_HEADER(<FILEPATH> <MAJOR> <MINOR> <PATCH>) ####
-# @brief				Creates a copy of the version.h.in header in the caller's source directory.
-#\n						You can optionally include the path to the input version header.
-# @param _out_header	The name & location of the output file.
-# @param _major			Major version number
-# @param _minor			Minor version number
-# @param _patch			Patch version number
-function(CREATE_VERSION_HEADER _out_header _name _major _minor _patch)
-	# Set temporary variables
-	set(IN_NAME "${_name}" CACHE STRING "" FORCE)
-	set(IN_MAJOR "${_major}" CACHE STRING "" FORCE)
-	set(IN_MINOR "${_minor}" CACHE STRING "" FORCE)
-	set(IN_PATCH "${_patch}" CACHE STRING "" FORCE)
-
-	# Remove previous
-	file(REMOVE "${_out_header}")
-
-	# Configure file
-	configure_file("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/input/version.h.in" "${_out_header}")
-
-	# Unset temporary cache variables
-	unset(IN_NAME CACHE)
-	unset(IN_MAJOR CACHE)
-	unset(IN_MINOR CACHE)
-	unset(IN_PATCH CACHE)
-endfunction()
-
 #### IS_GIT_REPOSITORY(<OUT_BOOL> <REPOSITORY_ROOT_DIRECTORY>) ####
 # @brief	Checks if the given directory contains a ".git" subdirectory.
 function(IS_GIT_REPOSITORY _out_var _path)
@@ -165,4 +140,120 @@ function(GET_VERSION _version_prefix _repository_path)
 	unset(_path)
 	unset(USE_AUTOVERSION CACHE)
 	unset(_use_env)
+endfunction()
+
+
+#### CREATE_VERSION_HEADER(<FILEPATH> <MAJOR> <MINOR> <PATCH>) ####
+# @brief				Creates a copy of the version.h.in header in the caller's source directory.
+#\n						You can optionally include the path to the input version header.
+# @param _out_header	The name & location of the output file.
+# @param _major			Major version number
+# @param _minor			Minor version number
+# @param _patch			Patch version number
+function(CREATE_VERSION_HEADER _out_header _name _major _minor _patch)
+	# Set temporary variables
+	set(IN_NAME "${_name}" CACHE STRING "" FORCE)
+	set(IN_MAJOR "${_major}" CACHE STRING "" FORCE)
+	set(IN_MINOR "${_minor}" CACHE STRING "" FORCE)
+	set(IN_PATCH "${_patch}" CACHE STRING "" FORCE)
+
+	# Remove previous
+	file(REMOVE "${_out_header}")
+
+	# Configure file
+	configure_file("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/input/version.h.in" "${_out_header}")
+
+	# Unset temporary cache variables
+	unset(IN_NAME CACHE)
+	unset(IN_MAJOR CACHE)
+	unset(IN_MINOR CACHE)
+	unset(IN_PATCH CACHE)
+endfunction()
+
+
+										###########################################
+										#              Autoversion 2              #
+										###########################################
+
+#### AV2_PARSE_TAG(<OUT_LIST> <GIT_TAG>) ####
+# @brief	Retrieve all contiguous valid C-locale word characters as a list of strings.
+macro(AV2_PARSE_TAG _out_list _tag)
+	message(STATUS "AV2_PARSE_TAG():  \$\{_tag\} = \"${_tag}\"")
+	if ("${_tag}" STREQUAL "")
+		message(FATAL_ERROR "AV2_PARSE_TAG() [ERROR]:  Received empty git tag!")
+	endif()
+	string(REGEX MATCHALL "${AUTOVERSION_2_REGEX_VALIDTAGCHARACTERS}" "${_out_list}" "${_tag}")
+	message(STATUS "AV2_PARSE_TAG():  \$\{_out_list\} = \"${${_out_list}}\"")
+	set("${_out_list}" "${${_out_list}}" PARENT_SCOPE)
+endmacro()
+
+#### AV2_GET_VERSION(<ROOT_REPO_DIR> <OUT_VERSION> [OUT_GROUP1] [OUT_GROUP2] ...) ####
+function(AV2_GET_VERSION _working_dir _out_fullversion)
+	AV_GET_GIT_TAG("${_working_dir}" _tmp)
+	AV2_PARSE_TAG(_split_tag "${_tmp}")
+
+	# Remove all alpha characters
+	string(REGEX REPLACE "[\\-\\.]*[A-Za-z]+[\\-\\.]*" "" _tmp ${_tmp})
+	string(REGEX REPLACE "(^[0-9]+[\\-\\.][0-9]+[\\-\\.][0-9]+)(.+)" "\\1" _tmp "${_tmp}")
+
+	set("${_out_fullversion}" "${_tmp}" PARENT_SCOPE)
+
+	message(STATUS "AV2_GET_VERSION():  \$\{${_out_fullversion}\} = \"${${_out_fullversion}}\"")
+
+	# Return early if there aren't any additional args
+	if (${ARGC} EQUAL 2)
+		return()
+	endif()
+
+	# Iterate through all additional arguments
+	math(EXPR _i "0")
+	foreach(_out_group IN LISTS ARGN)
+		list(GET _split_tag ${_i} _tag_group)
+		set("${_out_group}" "${_tag_group}" PARENT_SCOPE)
+		math(EXPR _i "${_i} + 1")
+	endforeach()
+
+	set("${_out_fullversion}" "${${_out_fullversion}}" PARENT_SCOPE)
+endfunction()
+
+
+#### GET_VERSION_2(<PROJECT_PREFIX> <ROOT_REPO_DIR> [GROUP1_SUFFIX] [GROUP2_SUFFIX] ...) ####
+# @brief					Creates version variables for the given project name.
+# @param _project_prefix	The prefix to insert before each version variable name.
+# @param _repository_path	The root directory of the target repository.
+# @param ...				Suffixes to append to: "${_project_prefix}_VERSION"
+function(GET_VERSION_2 _project_prefix _repository_path)
+	# Check if this is a git repository
+	IS_GIT_REPOSITORY(USE_AUTOVERSION ${_repository_path})
+
+	# If this is a git repository
+	if (USE_AUTOVERSION)
+		include(PrependEach)
+		message(STATUS "GET_VERSION_2():  \$\{ARGN\} = \"${ARGN}\"")
+		PREPEND_EACH(_version_groups "${ARGN}" "${_project_prefix}_VERSION_")
+		message(STATUS "GET_VERSION_2():  Version Groups = \"${_version_groups}\"")
+		AV2_GET_VERSION("${_repository_path}" "${_project_prefix}_VERSION" ${_split_version})
+		if (${_project_prefix}_VERSION STREQUAL "")
+			set(_use_env TRUE)
+			message(STATUS "GET_VERSION_2():  Version string is blank, falling back to environment variable.")
+		else()
+			set(_use_env FALSE)
+		endif()
+	endif()
+
+	# If this isn't a git repository, or if the version variable is empty
+	if(NOT USE_AUTOVERSION OR _use_env)
+		set(_env_var_name "${_project_prefix}_VERSION")
+		message(WARNING
+			" > AutoVersion2 couldn't find a usable git tag, and is falling back to AutoVersion1 behaviour!\n"
+			" Attempting to fallback to environment variable: \"${_env_var_name}\"\n"
+		)
+		set(${_version_prefix}_VERSION "$ENV{${_env_var_name}}" PARENT_SCOPE)
+		AV_PARSE_TAG("${${_env_var_name}}" ${_env_var_name}_MAJOR ${_env_var_name}_MINOR ${_env_var_name}_PATCH)
+	endif()
+
+
+	# Cleanup variables
+	unset(USE_AUTOVERSION CACHE)
+	unset(_version_groups CACHE)
 endfunction()
--- PrependEach.cmake
+++ PrependEach.cmake
@@ -1,16 +1,31 @@
 # 307modules/PrependEach.cmake
 cmake_minimum_required(VERSION 3.15)
 
-#### PREPEND_EACH ####
+#### STRIP_STRING(<STRING>) ####
+# @brief	Inline function that removes trailing newlines and leading/trailing spaces.
+macro(STRIP_STRING _string)
+	message(STATUS "STRIP_STRING():  \$\{_string\} = \"${_string}\"")
+	string(REGEX REPLACE "\n$" "" _string "${_string}")
+	string(STRIP "${_string}" _string)
+	string(REGEX REPLACE "\n$" "" _string "${_string}")
+	message(STATUS "STRIP_STRING():  \$\{_string\} = \"${_string}\"")
+endmacro()
+
+#### PREPEND_EACH(<OUT_LIST_NAME> <IN_LIST> <PREPEND_STRING>) ####
 # @brief			Prepend each element in a list with a given string.
 #					Most useful for converting a list of relative paths to absolute paths. (target_sources)
 # @param _out_list	Output List Name
 # @param _in_list	Input List
 # @param _prepend	The string to prepend to each element in ${_in_list}. (FOR FILEPATHS, USE A TRAILING SLASH!)
-function(PREPEND_EACH _out_list _in_list _prepend)
-	set(tmp_list "")
-	foreach(_target_path IN LISTS ${_in_list})
-		list(APPEND tmp_list "${_prepend}${_target_path}")
+function(PREPEND_EACH _out _in_list _prepend)
+	set(${_out})
+	message(STATUS "PREPEND_EACH():  \$\{_in_list\} = \"${_in_list}\"")
+	foreach(_target_path IN LISTS _in_list)
+		set(_tmp "${_prepend}${_target_path}")
+		list(APPEND ${_out} "${_tmp}")
+		message(STATUS "PREPEND_EACH():  Appended \"${_tmp}\" to \"\$\{${_out_list}\}\".")
 	endforeach()
-	set(${_out_list} "${tmp_list}" CACHE STRING "" FORCE)
+	list(JOIN "${tmp_list}" ";" _tmp)
+	set(${_out_list} "${${_out_list}}" PARENT_SCOPE)
+	message(STATUS "PREPEND_EACH():  \$\{_out_list\} = \"${${_out_list}}\"")
 endfunction()
