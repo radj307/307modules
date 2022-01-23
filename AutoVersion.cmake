062ca59f54d2c30457533aad2b04982ac2d803b5
radj307 <radj307@gmail.com>
2022-01-23 13:34:19 -0500

    fix some bugs

--- AutoVersion.cmake
+++ AutoVersion.cmake
@@ -190,13 +190,19 @@ endmacro()
 #### AV2_GET_VERSION(<ROOT_REPO_DIR> <OUT_VERSION> [OUT_GROUP1] [OUT_GROUP2] ...) ####
 function(AV2_GET_VERSION _working_dir _out_fullversion)
 	AV_GET_GIT_TAG("${_working_dir}" _tmp)
+	
+	message(STATUS "AV2_GET_VERSION():  Git tag = \"${_tmp}\"")
+
 	AV2_PARSE_TAG(_split_tag "${_tmp}")
+
+	message(STATUS "AV2_GET_VERSION():  Split tag = \"${_split_tag}\"")
 
 	# Remove all alpha characters
 	string(REGEX REPLACE "[\\-\\.]*[A-Za-z]+[\\-\\.]*" "" _tmp ${_tmp})
 	string(REGEX REPLACE "(^[0-9]+[\\-\\.][0-9]+[\\-\\.][0-9]+)(.+)" "\\1" _tmp "${_tmp}")
 
-	set("${_out_fullversion}" "${_tmp}" PARENT_SCOPE)
+	set("${_out_fullversion}" "${_tmp}")
+	set("${_out_fullversion}" "${${_out_fullversion}}" CACHE STRING "" FORCE)
 
 	message(STATUS "AV2_GET_VERSION():  \$\{${_out_fullversion}\} = \"${${_out_fullversion}}\"")
 
@@ -209,11 +215,9 @@ function(AV2_GET_VERSION _working_dir _out_fullversion)
 	math(EXPR _i "0")
 	foreach(_out_group IN LISTS ARGN)
 		list(GET _split_tag ${_i} _tag_group)
-		set("${_out_group}" "${_tag_group}" PARENT_SCOPE)
+		set("${_out_group}" "${_tag_group}" CACHE STRING "" FORCE)
 		math(EXPR _i "${_i} + 1")
 	endforeach()
-
-	set("${_out_fullversion}" "${${_out_fullversion}}" PARENT_SCOPE)
 endfunction()
 
 
@@ -232,7 +236,7 @@ function(GET_VERSION_2 _project_prefix _repository_path)
 		message(STATUS "GET_VERSION_2():  \$\{ARGN\} = \"${ARGN}\"")
 		PREPEND_EACH(_version_groups "${ARGN}" "${_project_prefix}_VERSION_")
 		message(STATUS "GET_VERSION_2():  Version Groups = \"${_version_groups}\"")
-		AV2_GET_VERSION("${_repository_path}" "${_project_prefix}_VERSION" ${_split_version})
+		AV2_GET_VERSION("${_repository_path}" "${_project_prefix}_VERSION" ${_version_groups})
 		if (${_project_prefix}_VERSION STREQUAL "")
 			set(_use_env TRUE)
 			message(STATUS "GET_VERSION_2():  Version string is blank, falling back to environment variable.")
@@ -251,9 +255,32 @@ function(GET_VERSION_2 _project_prefix _repository_path)
 		set(${_version_prefix}_VERSION "$ENV{${_env_var_name}}" PARENT_SCOPE)
 		AV_PARSE_TAG("${${_env_var_name}}" ${_env_var_name}_MAJOR ${_env_var_name}_MINOR ${_env_var_name}_PATCH)
 	endif()
+endfunction()
 
 
-	# Cleanup variables
-	unset(USE_AUTOVERSION CACHE)
-	unset(_version_groups CACHE)
+#### CREATE_VERSION_HEADER(<FILEPATH> <MAJOR> <MINOR> <PATCH>) ####
+# @brief				Creates a copy of the version.h.in header in the caller's source directory.
+#\n						You can optionally include the path to the input version header.
+# @param _out_header	The name & location of the output file.
+# @param _major			Major version number
+# @param _minor			Minor version number
+# @param _patch			Patch version number
+function(CREATE_VERSION_HEADER_2 _out_header _PROJECT _MAJOR _MINOR _PATCH _EXTRA)
+	# Remove previous
+	file(REMOVE "${_out_header}")
+
+	message(STATUS "CREATE_VERSION_HEADER_2():  Project = \"${_PROJECT}\"")
+	message(STATUS "CREATE_VERSION_HEADER_2():  Major = \"${_MAJOR}\"")
+	message(STATUS "CREATE_VERSION_HEADER_2():  Minor = \"${_MINOR}\"")
+	message(STATUS "CREATE_VERSION_HEADER_2():  Patch = \"${_PATCH}\"")
+	message(STATUS "CREATE_VERSION_HEADER_2():  Extra = \"${_EXTRA}\"")
+
+	set(_PROJECT "${_PROJECT}" CACHE STRING "" FORCE)
+	set(_MAJOR "${_MAJOR}" CACHE STRING "" FORCE)
+	set(_MINOR "${_MINOR}" CACHE STRING "" FORCE)
+	set(_PATCH "${_PATCH}" CACHE STRING "" FORCE)
+	set(_EXTRA "${_EXTRA}" CACHE STRING "" FORCE)
+
+	# Configure file
+	configure_file("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/input/version.h_2.in" "${_out_header}")
 endfunction()
