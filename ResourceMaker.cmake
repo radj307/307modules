bb8ed1a0bf8f264b929e6ade93e067eb9e60f038
radj307 <radj307@gmail.com>
2022-10-31 21:10:00 -0400

    add `MAKE_COPYRIGHT_HEADER` function

--- ResourceMaker.cmake
+++ ResourceMaker.cmake
@@ -229,6 +229,35 @@ function(MAKE_RESOURCE _out_file)
 	message(STATUS "MAKE_RESOURCE():  Resource file written successfully.")
 endfunction(MAKE_RESOURCE)
 
+# MAKE_COPYRIGHT_HEADER(<OUT_FILE> <PROJECT_NAME> <COPYRIGHT_YEAR> <COPYRIGHT_HOLDER>)
+#	Creates a header file containing copyright notices as preprocessor definitions.
+#	This is intended for POSIX-based systems as an alternative to the VERSIONINFO resource on Windows.
+# PARAMETERS:
+#	_out_header			The output header file.
+#	_project_name		The name of the project. This determines the names of the processor definitions.
+#	_copyright_year		The year that the copyright notice was last updated.
+#	_copyright_holder	The holder of the copyright notice.
+# PREPROCESSOR DEFINITIONS:
+#	${_project_name}_COPYRIGHT_YEAR		The year that the copyright notice was last updated.
+#	${_project_name}_COPYRIGHT_HOLDER	The owner of the copyright.
+#	${_project_name}_COPYRIGHT			The full copyright notice as a string.
+function(MAKE_COPYRIGHT_HEADER _out_header _project_name _copyright_year _copyright_holder)
+	message(STATUS
+		" MAKE_COPYRIGHT_HEADER():	_out_header			= \"${_out_header}\""
+		" MAKE_COPYRIGHT_HEADER():	_project_name		= \"${_project_name}\""
+		" MAKE_COPYRIGHT_HEADER():	_copyright_year		= \"${_copyright_year}\""
+		" MAKE_COPYRIGHT_HEADER():	_copyright_holder	= \"${_copyright_holder}\""
+	)
+
+	file(REMOVE "${_out_header}")
+
+	set(IN_PROJECT "${_project_name}" CACHE INTERNAL)
+	set(IN_YEAR "${_copyright_year}" CACHE INTERNAL)
+	set(IN_HOLDER "${_copyright_holder}" CACHE INTERNAL)
+
+	configure_file("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/input/copyright.h.in" "${_out_header}" USE_SOURCE_PERMISSIONS @ONLY)
+endfunction()
+
 
 #############################################################################
 #                     ResourceMaker Legacy Functions						#
